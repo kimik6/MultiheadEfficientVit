6e5b95b7e328b210d4ee42c3dac3840aacd4bee4
Kimia Kamiyab <72387583+kimik6@users.noreply.github.com>
2024-08-05 14:55:12 +0330

    Update seg.py

--- model/seg.py
+++ model/seg.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from backbone import EfficientViTBackbone, EfficientViTLargeBackbone
-from nn import (
+from model.nn import (
     ConvLayer,
     DAGBlock,
     FusedMBConv,
