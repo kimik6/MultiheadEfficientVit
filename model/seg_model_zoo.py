cd578bc491d89d1ec23059e3967d13aba9ec4ce7
kimik6 <kakimia595@gmail.com>
2024-09-09 11:59:08 +0330

    bdd single tast fine tune

--- model/seg_model_zoo.py
+++ model/seg_model_zoo.py
@@ -5,6 +5,9 @@
 from model.seg import (
     EfficientViTSeg,
     efficientvit_seg_b0,
+    efficientvit_seg_b1,
+    efficientvit_seg_b2,
+    efficientvit_seg_b3,
 )
 from model.nn.norm import set_norm_eps
 from model.utils.network import load_state_dict_from_file
@@ -15,16 +18,22 @@ __all__ = ["create_seg_model"]
 REGISTERED_SEG_MODEL: dict[str, dict[str, str]] = {
     "bdd": {
         "b0": "assets/checkpoints/seg/cityscapes/b0.pt",
+        "b1": "assets/checkpoints/seg/cityscapes/b1.pt",
+        "b2": "assets/checkpoints/seg/cityscapes/b2.pt",
+        "b3": "assets/checkpoints/seg/cityscapes/b3.pt",
     },
 
 }
 
 
 def create_seg_model(
-    name: str, dataset: str, multitask: str, pretrained=True, weight_url: str or None = None, **kwargs
+    name: str, dataset: str, multitask: str, pretrained=True, just_bb=False, weight_url: str or None = None, **kwargs
 ) -> EfficientViTSeg:
     model_dict = {
         "b0": efficientvit_seg_b0,
+        "b1": efficientvit_seg_b1,
+        "b2": efficientvit_seg_b2,
+        "b3": efficientvit_seg_b3,
     }
 
     model_id = name.split("-")[0]
@@ -37,10 +46,13 @@ def create_seg_model(
         set_norm_eps(model, 1e-7)
 
     if pretrained:
-        weight_url = weight_url or REGISTERED_SEG_MODEL[dataset].get(name, None)
-        if weight_url is None:
-            raise ValueError(f"Do not find the pretrained weight of {name}.")
+        if not just_bb:
+            weight_url = weight_url or REGISTERED_SEG_MODEL[dataset].get(name, None)
+            if weight_url is None:
+                raise ValueError(f"Do not find the pretrained weight of {name}.")
+            else:
+                weight = load_state_dict_from_file(weight_url)
+                model.load_state_dict(weight)
         else:
-            weight = load_state_dict_from_file(weight_url)
-            model.load_state_dict(weight)
+            pass
     return model
