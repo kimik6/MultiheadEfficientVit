b92ca4da241b0330adfa72796d2f7bcbe3892091
kimik6 <kakimia595@gmail.com>
2024-09-09 14:23:29 +0330

    bdd single tast fine tune

--- fine_tune_train.py
+++ fine_tune_train.py
@@ -34,9 +34,9 @@ def train_net(args):
     backbone_weight_url = args.backbone_weight_url ,
     engine = args.engine
     if pretrained is not None:
-        model = create_seg_model(args.model, 'bdd',args.task,weight_url=pretrained)
+        model = create_seg_model(args.model, 'bdd',args.task,None,weight_url=pretrained)
     elif backbone_weight_url is not None:
-        model = create_seg_model(args.model, 'bdd',args.task,backbone_weight_url=backbone_weight_url)
+        model = create_seg_model(args.model, 'bdd',args.task,backbone_weight_url=backbone_weight_url,weight_url=None)
     else:
         model = create_seg_model(args.model, 'bdd',args.task, False)
 
