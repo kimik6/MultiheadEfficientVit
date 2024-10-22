e81f9c22a991ad31a93ed128af74d97bb4418cc2
kimik6 <kakimia595@gmail.com>
2024-10-22 12:24:03 +0330

    tusimple fine tune

--- loss.py
+++ loss.py
@@ -42,9 +42,9 @@ class TotalLoss(nn.Module):
         # _, seg_ll = torch.max(seg_ll, 1)
         seg_ll = seg_ll
         _, out_ll = torch.max(out_ll, 1)
-        out_ll-out_ll.long()
-        print('seg label shape = ',seg_ll.shape)
-        print('seg output shape = ',out_ll.shape)
+        # print('seg label shape = ',seg_ll.shape)
+        # print('seg output shape = ',out_ll.shape)
+    
 
         if task == 'multi':
             tversky_loss = self.seg_tver_da(out_da, seg_da) + self.seg_tver_ll(out_ll, seg_ll)
