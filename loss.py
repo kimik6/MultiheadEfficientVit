a1d899d74b1039fa07303d26d52a1da3ddcd8de5
kimik6 <kakimia595@gmail.com>
2024-10-22 13:36:11 +0330

    tusimple fine tune

--- loss.py
+++ loss.py
@@ -38,9 +38,9 @@ class TotalLoss(nn.Module):
         seg_da, seg_ll = targets
         out_da, out_ll = outputs
         # _, seg_da = torch.max(seg_da, 1)
-        seg_da = torch.Tensor(seg_da)
+        seg_da = seg_da
         # _, seg_ll = torch.max(seg_ll, 1)
-        seg_ll = torch.Tensor(seg_ll)
+        seg_ll = seg_ll
         out_ll=out_ll[:,1]
 
         if task == 'multi':
